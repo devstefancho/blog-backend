85fe855d788f769418e64ffe52d12680e142ec62
devstefancho <skku7714@gmail.com>
2024-01-16 00:08:14 +0900

    fix: fix test case (only for markdown files)

--- src/__test/util.spec.ts
+++ src/__test/util.spec.ts
@@ -5,10 +5,11 @@ import { recursiveReadDir } from 'src/util/util';
 beforeEach(() => {
   mockFs({
     mock: {
-      'file1.txt': 'This is file 1',
-      'file2.txt': 'This is file 2',
+      'file1.md': 'This is file 1',
+      'file2.md': 'This is file 2',
+      'file3.txt': 'This is file 3 text file',
       subdir: {
-        'file3.txt': 'This is file 3',
+        'file3.md': 'This is file 3',
       },
     },
   });
@@ -21,11 +22,20 @@ afterEach(() => {
 test('should list all files in a directory recursively', async () => {
   const directoryPath = path.join(__dirname, '../../.');
   const expectedFiles = [
-    { path: path.join(directoryPath, 'mock/file1.txt'), name: 'file1.txt' },
-    { path: path.join(directoryPath, 'mock/file2.txt'), name: 'file2.txt' },
     {
-      path: path.join(directoryPath, 'mock/subdir', 'file3.txt'),
-      name: 'file3.txt',
+      path: path.join(directoryPath, 'mock/file1.md'),
+      name: 'file1.md',
+      slug: 'file1',
+    },
+    {
+      path: path.join(directoryPath, 'mock/file2.md'),
+      name: 'file2.md',
+      slug: 'file2',
+    },
+    {
+      path: path.join(directoryPath, 'mock/subdir', 'file3.md'),
+      name: 'file3.md',
+      slug: 'file3',
     },
   ].sort();
 
