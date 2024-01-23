f9e247bb86dd91e12b32362fbcfe0707810c55ff
devstefancho <skku7714@gmail.com>
2024-01-24 00:24:13 +0900

    fix: pass test code

--- src/visit/visit.controller.spec.ts
+++ src/visit/visit.controller.spec.ts
@@ -1,6 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VisitController } from './visit.controller';
 import { VisitService } from './visit.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Visit } from './entities/visit.entity';
+
+class MockVisitRepository {
+  save = jest.fn();
+  count = jest.fn();
+  findOne = jest.fn();
+  update = jest.fn();
+  remove = jest.fn();
+}
 
 describe('VisitController', () => {
   let controller: VisitController;
@@ -8,7 +18,13 @@ describe('VisitController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VisitController],
-      providers: [VisitService],
+      providers: [
+        VisitService,
+        {
+          provide: getRepositoryToken(Visit),
+          useValue: MockVisitRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<VisitController>(VisitController);
--- src/visit/visit.service.spec.ts
+++ src/visit/visit.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VisitService } from './visit.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Visit } from './entities/visit.entity';
+
+class MockVisitRepository {
+  save = jest.fn();
+  count = jest.fn();
+  findOne = jest.fn();
+  update = jest.fn();
+  remove = jest.fn();
+}
 
 describe('VisitService', () => {
   let service: VisitService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VisitService],
+      providers: [
+        VisitService,
+        {
+          provide: getRepositoryToken(Visit),
+          useValue: MockVisitRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<VisitService>(VisitService);
